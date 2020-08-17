ba6616e29575f9c798572da24aaf799bd827abc4
Piotr Marczuk <pmz@kmd.dk>
2020-08-17 14:37:28 +0200

    Deleted old code not related to workshop

--- src/UnitTestMockingExamples.BusinessLogic/Responses/SimpleWithNameResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UnitTestMockingExamples.Responses
-{
-    public class SimpleWithNameResponse
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; }
-    }
-}
